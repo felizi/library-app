f6b9a0de9d8520d8ef042b296cbb4bcd8df5054d
Daniel Felizi <felizi.daniel@gmail.com>
2016-06-05 20:26:07 -0300

    Adjustments by JSHint and JSCS

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ app.use(express.static('public'));
 app.use(express.static('src/views'));
 
 app.get('/', function(req, res){
-  res.send("Hello world");
-})
+  res.send('Hello world');
+});
 
 app.get('/books', function(req, res){
-  res.send("Hello books");
-})
+  res.send('Hello books');
+});
 
 app.listen(port, function(err){
     console.log('Running server on port ' + port);
