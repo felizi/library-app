2acb7f98cf807bed3da48a6b8be06ed51b79812f
Daniel Felizi <felizi.daniel@gmail.com>
2016-06-05 20:34:26 -0300

    Code style with JSCS

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ var port = 5000;
 app.use(express.static('public'));
 app.use(express.static('src/views'));
 
-app.get('/', function(req, res){
-  res.send('Hello world');
+app.get('/', function(req, res) {
+    res.send('Hello world');
 });
 
-app.get('/books', function(req, res){
-  res.send('Hello books');
+app.get('/books', function(req, res) {
+    res.send('Hello books');
 });
 
-app.listen(port, function(err){
+app.listen(port, function(err) {
     console.log('Running server on port ' + port);
 });
