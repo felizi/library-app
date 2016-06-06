2f74cfeaca34a6b0cde7f98bee269c12a2e2424b
Daniel Felizi <felizi.daniel@gmail.com>
2016-06-05 21:42:05 -0300

    Add Nodemon dependency and config that on gulpfile.js; Change config for port on app.js to get from nodemon;

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var nodemon = require('gulp-nodemon');
 
 var jsFiles = [ '*.js', 'src/**/*.js'];
 
@@ -34,4 +35,22 @@ gulp.task('inject', function () {
     .pipe(wiredep(options))
     .pipe(inject(injectSrc, injectOptions))
     .pipe(gulp.dest('./src/views'));
+});
+
+gulp.task('serve', ['style', 'inject'], function () {
+  var options = {
+    // When nodemon, execute script
+    script: 'app.js',
+    // Delay between restarts
+    delayTime: 1,
+    env: {
+      'PORT': 3000
+    },
+    // What the files nodemon will monitor
+    watch: jsFiles
+  };
+  return nodemon(options)
+    .on('restart', function (ev) {
+      console.log('Restarting...');
+    });
 });
