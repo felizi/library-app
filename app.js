8c63bb33163a9922a88558ee4f71a2848e3da4b7
Daniel Felizi <felizi.daniel@gmail.com>
2016-06-06 21:23:47 -0300

    Implementation of Jade template engine

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ var port = process.env.PORT || 5000;
 
 // Configura um local de arquivos estáticos (como CSS e JS).
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+// app.use(express.static('src/views'));
+app.set('views', './src/views');
+app.set('view engine', 'jade');
 
 app.get('/', function(req, res) {
-    res.send('Hello world');
+    res.render('index', {list: ['a','b']});
 });
 
 app.get('/books', function(req, res) {
